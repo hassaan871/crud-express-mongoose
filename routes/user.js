19070efcbdebdea9188f4f165f9dcaff21c31d86
Hassaan <87420500+hassaan871@users.noreply.github.com>
2024-10-26 17:56:41 +0500

    user router created

---
+++ routes/user.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const { handleGetAllUsers, handleCreateNewUser, handleGetUserById, handleUpdateUserById, handleDeleteUserById } = require('../controllers/user')
+
+
+
+const router = express.Router()
+
+
+router.route('/')
+.get(handleGetAllUsers)
+.post(handleCreateNewUser)
+
+router.route('/:id')
+.get(handleGetUserById)
+.patch(handleUpdateUserById)
+.delete(handleDeleteUserById)
+
+module.exports = router;
