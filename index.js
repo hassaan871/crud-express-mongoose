61aa546e6ed9465d779d9b333dcfabec72ac205e
Hassaan <87420500+hassaan871@users.noreply.github.com>
2024-10-26 18:16:23 +0500

    user routes created, routes middleware included in index.js along with urlendcoded middleware

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
 const express = require('express');
 const {connectMongoDB} = require('./connection');
+const userRouter = require('./routes/user')
 
 const app = express();
 const port = 8000;
 
+//Connection
 connectMongoDB('mongodb://127.0.0.1:27017/learning')
 .then(()=> console.log("MongoDB Connected"))
 .catch((err) => console.log("Mongo Error" , err))
+
+//Middlewares
+app.use(express.urlencoded({extended: false}))
+
+
+//Routes
+app.use('/api/users', )
 
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
