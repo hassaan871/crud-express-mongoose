1dd36ca79252785a226309bf5c49579546f9ddfa
Hassaan <87420500+hassaan871@users.noreply.github.com>
2024-10-26 16:41:22 +0500

    user model created

---
+++ models/user.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose')
+
+const userSchema = new mongoose.Schema({
+    firstName: {
+        type: String,
+        required : true
+    },
+    lastName: {
+        type: String
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    jobTitle: {
+        type: String
+    },
+})
+
+const User = mongoose.model('users', userSchema)
+
+module.exports = User 
